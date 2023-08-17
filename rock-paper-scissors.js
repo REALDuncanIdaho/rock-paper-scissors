54ce352efc8cd807b455c1f5162b8926c38fa437
REALDuncanIdaho <139713456+REALDuncanIdaho@users.noreply.github.com>
2023-08-17 16:15:20 -0400

    created multiple functions

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,7 +3,7 @@ prompt user for rock, paper, or scissors
 determine of user or computer wins
 return message based on outcome of command */
 
-let computerSelection = Math.floor(Math.random() * 3);
+/*let computerSelection = Math.floor(Math.random() * 3);
   if (computerSelection == 0) {
     computerChoice = "rock";
   }
@@ -23,8 +23,10 @@ let playerChoice = prompt("Rock, Paper, or Scissors?");
     playerSelection = 1;
   }
   else if (playerChoice == "scissors") {
-    playerSelection = 2;
-  }
+    playerSelection = 2; 
+  } else if (playerChoice != 'rock' || 'paper' || 'scissors') {
+    alert("You didn't enter rock, paper, or scissors.")
+ }
 
   function playRound() {
     if (playerSelection == 0 && computerSelection == 2 || playerSelection > computerSelection) {
@@ -35,11 +37,83 @@ let playerChoice = prompt("Rock, Paper, or Scissors?");
     }
     else if (playerSelection == computerSelection) {
       alert("You and the Computer both entered "+playerChoice+". It's a tie. Try again.");
-    } else if (playerChoice != 'rock' || 'paper' || 'scissors') {
-      alert("You didn't enter rock, paper, or scissors.")
-   }
+    }
   }
 
 console.log(computerSelection)
 console.log(playerSelection)
-playRound()
+playRound() */
+
+let playerChoice;
+let playerSelection;
+let computerChoice;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerSelection() {
+  let computerSelection = Math.floor(Math.random() * 3);
+  
+  console.log(computerSelection);
+  return(computerSelection);
+}  
+
+computerSelection = getComputerSelection();
+
+
+function getComputerChoice() {
+  if (computerSelection == 0) {
+    computerChoice = "rock";
+  }
+  else if (computerSelection == 1) {
+    computerChoice = "paper";
+  }
+  else if (computerSelection == 2) {
+    computerChoice = "scissors";
+  }
+  console.log(computerChoice);
+  return(computerChoice);
+}
+
+computerChoice = getComputerChoice();
+
+function getPlayerChoice() {
+  let playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+  console.log(playerChoice);
+  return (playerChoice);
+}
+playerChoice = getPlayerChoice();
+
+
+function getPlayerSelection() {
+    if (playerChoice == "rock") {
+    playerSelection = 0;
+  }
+  else if (playerChoice == "paper") {
+    playerSelection = 1;
+  }
+  else if (playerChoice == "scissors") {
+    playerSelection = 2;
+  }
+  console.log(playerSelection);
+  return (playerSelection);
+  }
+playerSelection = getPlayerSelection();
+
+
+function playRound() {
+  if (playerSelection == 0 && computerSelection == 2 || playerSelection > computerSelection) {
+    alert('The Computer entered '+computerChoice+' and you entered '+playerChoice+'. You Win!');
+  }
+  else if (playerSelection == 2 && computerSelection == 0 || playerSelection < computerSelection) {
+    alert('The Computer entered '+computerChoice+' and you entered '+playerChoice+'. You Lose.');
+  }
+  else if (playerSelection == computerSelection) {
+    alert("You and the Computer both entered "+playerChoice+". It's a tie. Try again.");
+  } else if (playerChoice != 'rock' || 'paper' || 'scissors') {
+    alert("You didn't enter rock, paper, or scissors. Shame on you.")
+  }
+}
+
+playRound();
