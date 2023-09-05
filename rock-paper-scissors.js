a9798b92fb4e702fabd778bfed8a83d5359b7e6a
REALDuncanIdaho <139713456+REALDuncanIdaho@users.noreply.github.com>
2023-09-05 12:57:15 -0400

    updated UI

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,130 +3,6 @@ prompt user for rock, paper, or scissors
 determine of user or computer wins
 return message based on outcome of command */
 
-/*let computerSelection = Math.floor(Math.random() * 3);
-  if (computerSelection == 0) {
-    computerChoice = "rock";
-  }
-  else if (computerSelection == 1) {
-    computerChoice = "paper";
-  }
-  else if (computerSelection == 2) {
-    computerChoice = "scissors";
-  }
-  
-
-let playerChoice = prompt("Rock, Paper, or Scissors?");
-  if (playerChoice == "rock") {
-    playerSelection = 0;
-  }
-  else if (playerChoice == "paper") {
-    playerSelection = 1;
-  }
-  else if (playerChoice == "scissors") {
-    playerSelection = 2; 
-  } else if (playerChoice != 'rock' || 'paper' || 'scissors') {
-    alert("You didn't enter rock, paper, or scissors.")
- }
-
-  function playRound() {
-    if (playerSelection == 0 && computerSelection == 2 || playerSelection > computerSelection) {
-      alert('The Computer entered '+computerChoice+' and you entered '+playerChoice+'. You Win!')
-    }
-    else if (playerSelection == 2 && computerSelection == 0 || playerSelection < computerSelection) {
-      alert('The Computer entered '+computerChoice+' and you entered '+playerChoice+'. You Lose.')
-    }
-    else if (playerSelection == computerSelection) {
-      alert("You and the Computer both entered "+playerChoice+". It's a tie. Try again.");
-    }
-  }
-
-console.log(computerSelection)
-console.log(playerSelection)
-playRound() 
-
-let playerChoice;
-let playerSelection;
-let computerChoice;
-let computerSelection;
-let playerScore = 0;
-let computerScore = 0;
-
-function getComputerSelection() {
-  let computerSelection = Math.floor(Math.random() * 3);
-  
-  console.log(computerSelection);
-  return(computerSelection);
-}  
-
-computerSelection = getComputerSelection();
-
-
-function getComputerChoice() {
-  if (computerSelection == 0) {
-    computerChoice = "rock";
-  }
-  else if (computerSelection == 1) {
-    computerChoice = "paper";
-  }
-  else if (computerSelection == 2) {
-    computerChoice = "scissors";
-  }
-  console.log(computerChoice);
-  return(computerChoice);
-}
-
-computerChoice = getComputerChoice();
-
-function getPlayerChoice() {
-  let playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-
-  console.log(playerChoice);
-  return (playerChoice);
-}
-playerChoice = getPlayerChoice();
-
-
-function getPlayerSelection() {
-    if (playerChoice == "rock") {
-    playerSelection = 0;
-  }
-  else if (playerChoice == "paper") {
-    playerSelection = 1;
-  }
-  else if (playerChoice == "scissors") {
-    playerSelection = 2;
-  }
-  console.log(playerSelection);
-  return (playerSelection);
-  }
-playerSelection = getPlayerSelection();
-
-
-function playRound() {
-  if (playerSelection == 0 && computerSelection == 2 || playerSelection > computerSelection) {
-    alert('The Computer entered '+computerChoice+' and you entered '+playerChoice+'. You Win!');
-    playerScore++;
-  }
-  else if (playerSelection == 2 && computerSelection == 0 || playerSelection < computerSelection) {
-    alert('The Computer entered '+computerChoice+' and you entered '+playerChoice+'. You Lose.');
-    computerScore++;
-  }
-  else if (playerSelection == computerSelection) {
-    alert("You and the Computer both entered "+playerChoice+". It's a tie. Try again.");
-  } else if (playerChoice != 'rock' || 'paper' || 'scissors') {
-    alert("You didn't enter rock, paper, or scissors. Shame on you.")
-  }
-}
-
-
-function game() {
-  playRound();
-}
-
-game();
-console.log(playerScore);
-console.log(computerScore); */
-
 let playerChoice;
 let playerSelection;
 let computerChoice;
@@ -140,7 +16,6 @@ function getComputerSelection() {
   console.log('computerSelection' + computerSelection);
   getComputerChoice();
 }  
-
 
 function getComputerChoice() {
   if (computerSelection == 0) {
@@ -156,29 +31,59 @@ function getComputerChoice() {
   return(computerChoice);
 }
 
-function getPlayerChoice() {
-  playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+  function chooseRock() {
+      playerChoice = 'rock';
+      playerSelection = 0;
+      getComputerSelection();
+      getComputerChoice();
+      playRound();
+      document.getElementById("ps").innerHTML = 'Player Score: ' +playerScore;
+      document.getElementById("cs").innerHTML = 'Computer Score: ' +computerScore; 
+      if (playerScore == 5) {
+        alert("Congratulations! You've defeated the computer at a childrens game! You should feel really good about yourself!")
+      }  
+      else if (computerScore == 5) {
+        alert("You've been defeated by a computer at a childrens game. You should rethink a few things about your life.")
+      }
+  }
 
-  console.log('playerchoice' + playerChoice);
+  function choosePaper() {
+    playerChoice = 'paper';
+    playerSelection = 0;
+    getComputerSelection();
+    getComputerChoice();
+    playRound();
+    document.getElementById("ps").innerHTML = 'Player Score: ' +playerScore;
+    document.getElementById("cs").innerHTML = 'Computer Score: ' +computerScore;  
+    if (playerScore == 5) {
+      alert("Congratulations! You've defeated the computer at a childrens game! You should feel really good about yourself!")
+    }  
+    else if (computerScore == 5) {
+      alert("You've been defeated by a computer at a childrens game. You should rethink a few things about your life.")
+    } 
 }
 
-function getPlayerSelection() {
-    if (playerChoice == "rock") {
-    playerSelection = 0;
-  }
-  else if (playerChoice == "paper") {
-    playerSelection = 1;
-  }
-  else if (playerChoice == "scissors") {
-    playerSelection = 2;
-  }
-  console.log('playerSelection' + playerSelection);
-  return (playerSelection);
-  }
+function chooseScissors() {
+  playerChoice = 'scissors';
+  playerSelection = 0;
+  getComputerSelection();
+  getComputerChoice();
+  playRound();
+  document.getElementById("ps").innerHTML = 'Player Score: ' +playerScore;
+  document.getElementById("cs").innerHTML = 'Computer Score: ' +computerScore;  
+  if (playerScore == 5) {
+    alert("Congratulations! You've defeated the computer at a childrens game! You should feel really good about yourself!")
+  }  
+  else if (computerScore == 5) {
+    alert("You've been defeated by a computer at a childrens game. You should rethink a few things about your life.")
+  } 
+}
 
-
-function playRound() {
-  if (playerSelection == 0 && computerSelection == 2) {
+function playRound() { 
+  if (computerScore == 5 || playerScore == 5) {
+  computerScore == 0 && playerScore == 0;
+  }
+  else if (playerSelection == 0 && computerSelection == 2) {
     alert('The Computer entered '+computerChoice+' and you entered '+playerChoice+'. You Win!');
     playerScore++;
   }
@@ -197,31 +102,4 @@ function playRound() {
   else if (playerSelection == computerSelection) {
     alert("You and the Computer both entered "+playerChoice+". It's a tie. Try again.");
   } 
-  else if (playerChoice != 'rock' || 'paper' || 'scissors') {
-    alert("You didn't enter rock, paper, or scissors. Shame on you.")
-  }
 }
-
-function game() {
-  if (playerScore < 5 && computerScore < 5) {
-    getComputerSelection();
-    getPlayerChoice();
-    getPlayerSelection();
-    playRound();
-    console.log(playerScore);
-    console.log(computerScore);
-  }
-}
-
-
-
-game();
-game();
-game();
-game();
-game();
-game();
-game();
-game();
-game();
-game();
